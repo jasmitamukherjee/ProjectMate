57f7a670001747f6e0bfb654bcc82b2177b2a987
jasmitamukherjee <jasmita.mukherjee.btech2021@sitpune.edu.in>
2024-04-04 18:23:07 +0530

    created

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
+
+
+// -> "/"
+//home route
